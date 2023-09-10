49ccb7fe47646d90d2c056edbd49400a7be2de2a
Willi Kappler <willi.kappler.gm@gmail.com>
2023-09-10 17:57:34 +0200

    Add test case for encode and decode message

---
+++ tests/test_common.nim
@@ -0,0 +1,18 @@
+
+from chacha20 import Nonce, Key
+
+import nc_common
+
+block:
+    let data = "This is a secret message"
+    let nonceStr = "123456789012"
+    let keyStr = "12345678901234567890123456789012"
+
+    var nonce = cast[ptr(Nonce)](unsafeAddr(nonceStr[0]))
+    var key = cast[ptr(Key)](unsafeAddr(keyStr[0]))
+
+    let encodedData = ncEncodeMessage(data, key[], nonce[])
+    let decodedData = ncDecodeMessage(encodedData, key[], nonce[])
+
+    assert(data == decodedData)
+
