10662a8ab86324498cc6d74565779f518b716bf5
Willi Kappler <willi.kappler@uni-tuebingen.de>
2023-09-19 15:54:44 +0200

    Add default values for config file, add checks for key length

--- src/num_crunch/nc_config.nim
+++ src/num_crunch/nc_config.nim
@@ -13,18 +13,21 @@ type
         heartbeatTimeout*: uint16
         secretKey*: string
 
-func ncNewConfig*(): NCConfiguration =
+func ncNewConfig*(key: string): NCConfiguration =
     result.serverAddr = "127.0.0.1"
     result.serverPort = Port(3100)
     # Timeout for heartbeat messages set to 5 minutes
     result.heartbeatTimeout = 60 * 5
-    result.secretKey = ""
+    result.secretKey = key
+    assert(result.secretKey.len() == 32, "Key must be exactly 32 bytes long!")
 
-proc ncLoadConfig*(path: string): NCConfiguration {. raises: [IOError, ValueError, Exception] .}=
+proc ncLoadConfig*(path: string): NCConfiguration {.raises: [IOError, ValueError, Exception].}=
     let settings = loadConfig(path)
-    result.serverAddr = settings.getSectionValue("","server_address")
-    result.serverPort = Port(parseUInt(settings.getSectionValue("","server_port")))
-    result.heartbeatTimeout = uint16(parseUInt(settings.getSectionValue("","heartbeat_timeout")))
+    result.serverAddr = settings.getSectionValue("","server_address", "127.0.0.1")
+    result.serverPort = Port(parseUInt(settings.getSectionValue("","server_port", "3100")))
+    result.heartbeatTimeout = uint16(parseUInt(settings.getSectionValue("","heartbeat_timeout", "300")))
     result.secretKey = settings.getSectionValue("","secret_key")
+    assert(result.secretKey.len() == 32, "Key must be exactly 32 bytes long!")
     # Show all exceptions
     #{.effects.}
+
