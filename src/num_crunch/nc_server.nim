0a34828c1efa09497d6ed472e2bc843e055411f3
Willi Kappler <willi.kappler.gm@gmail.com>
2023-09-25 22:32:06 +0200

    Add more debug output and checks

--- src/num_crunch/nc_server.nim
+++ src/num_crunch/nc_server.nim
@@ -51,15 +51,21 @@ proc checkNodesHeartbeat[T](self: ptr NCServer[T]) {.thread.} =
     let timeOut = (uint(self.heartbeatTimeout) * 1000) + tolerance
 
     let heartbeatMessage = NCMessageToServer(kind: NCServerMsgKind.checkHeartbeat)
-    let serverSocket = newSocket()
 
     while not self.quit.load():
         sleep(int(timeOut))
 
         # Send message to server (self) so that it can check the heartbeats for all nodes
-        serverSocket.connect("127.0.0.1", self.serverPort)
-        ncSendMessageToServer(serverSocket, self.key, heartbeatMessage)
-        serverSocket.close()
+        echo("NCServer.checkNodesHeartbeat(), send heartbeat")
+        let serverSocket = newSocket()
+        try:
+            serverSocket.connect("127.0.0.1", self.serverPort)
+            ncSendMessageToServer(serverSocket, self.key, heartbeatMessage)
+            serverSocket.close()
+            echo("NCServer.checkNodesHeartbeat(), done")
+        except IOError:
+            echo("NCServer.checkNodesHeartbeat(), server doesn't respond, will exit now!")
+            break
 
 proc createNewNodeId[T](self: ptr NCServer[T]): NCNodeID =
     echo("NCServer.createNewNodeId()")
@@ -91,15 +97,23 @@ proc handleClient[T](tp: (ptr NCServer[T], Socket)) {.thread.} =
     let self = tp[0]
     let client = tp[1]
 
+    echo("NCServer.handleClient(), check pointers")
+
+    assert(self != nil)
+    assert(client != nil)
+
+    echo("NCServer.handleClient(), pointers OK")
+
     let (clientAddr, clientPort) = client.getPeerAddr()
-    echo(fmt("Connection from: {clientAddr}, port: {clientPort.uint16}"))
+    echo(fmt("NCServer.handleClient(), Connection from: {clientAddr}, port: {clientPort.uint16}"))
 
     let serverMessage = ncReceiveMessageFromNode(client, self.key)
+
 
     acquire(self.serverLock)
 
     if self.dataProcessor.isFinished():
-        echo("Work is done, exit now!")
+        echo("NCServer.handleClient(), Work is done, exit now!")
 
         let message = NCMessageToNode(kind: NCNodeMsgKind.quit)
         ncSendMessageToNode(client, self.key, message)
@@ -111,7 +125,7 @@ proc handleClient[T](tp: (ptr NCServer[T], Socket)) {.thread.} =
 
     case serverMessage.kind:
     of NCServerMsgKind.registerNewNode:
-        echo("Register new node")
+        echo("NCServer.handleClient(), Register new node")
 
         # Create a new node id and send it to the node
         let newId = self.createNewNodeId()
@@ -123,29 +137,29 @@ proc handleClient[T](tp: (ptr NCServer[T], Socket)) {.thread.} =
         self.nodes.add((newId, getTime()))
 
     of NCServerMsgKind.needsData:
-        echo("Node needs data")
+        echo("NCServer.handleClient(), Node needs data")
 
         if self.validNodeId(serverMessage.id):
-            echo("Node id valid: ", serverMessage.id)
+            echo("NCServer.handleClient(), Node id valid: ", serverMessage.id)
             # Send new data back to node
             let newData = self.dataProcessor.getNewData(serverMessage.id)
             let message = NCMessageToNode(kind: NCNodeMsgKind.newData, data: newData)
             ncSendMessageToNode(client, self.key, message)
         else:
-            echo("Node id invalid: ", serverMessage.id)
+            echo("NCServer.handleClient(), Node id invalid: ", serverMessage.id)
 
     of NCServerMsgKind.processedData:
-        echo("Node has processed data")
+        echo("NCServer.handleClient(), Node has processed data")
 
         if self.validNodeId(serverMessage.id):
-            echo("Node id valid: ", serverMessage.id)
+            echo("NCServer.handleClient(), Node id valid: ", serverMessage.id)
             # Store processed data from node
             self.dataProcessor.collectData(serverMessage.data)
         else:
-            echo("Node id invalid: ", serverMessage.id)
+            echo("NCServer.handleClient(), Node id invalid: ", serverMessage.id)
 
     of NCServerMsgKind.heartbeat:
-        echo("Node sends heartbeat")
+        echo("NCServer.handleClient(), Node sends heartbeat")
 
         for i in 0..<self.nodes.len():
             if self.nodes[i][0] == serverMessage.id:
@@ -153,7 +167,7 @@ proc handleClient[T](tp: (ptr NCServer[T], Socket)) {.thread.} =
                 break
 
     of NCServerMsgKind.checkHeartbeat:
-        echo("Check heartbeat times for all inodes")
+        echo("NCServer.handleClient(), Check heartbeat times for all inodes")
 
         let maxDuration = initDuration(seconds = int64(self.heartbeatTimeout))
 
@@ -161,16 +175,16 @@ proc handleClient[T](tp: (ptr NCServer[T], Socket)) {.thread.} =
 
         for n in self.nodes:
             if maxDuration < (currentTime - n[1]):
-                echo(fmt("Node is not sending heartbeat message: {n[0]}"))
+                echo(fmt("NCServer.handleClient(), Node is not sending heartbeat message: {n[0]}"))
                 # Let data processor know that this node seems dead
                 self.dataProcessor.maybeDeadNode(n[0])
 
     of NCServerMsgKind.getStatistics:
-        echo("Send some statistics")
+        echo("NCServer.handleClient(), Send some statistics")
         # TODO: respond with some information
 
     of NCServerMsgKind.forceQuit:
-        echo("Force quit")
+        echo("NCServer.handleClient(), Force quit")
         self.quit.store(true)
 
     release(self.serverLock)
@@ -197,13 +211,19 @@ proc runServer*[T](self: var NCServer[T]) =
 
     while not self.quit.load():
         serverSocket.acceptAddr(client, address)
-        createThread(clientThreadId, handleClient, (unsafeAddr(self), client))
-        clients.addLast(clientThreadId)
+        echo("runServer(), got new connection from node")
+        let selfPtr = unsafeAddr(self)
+        assert(selfPtr != nil)
+        createThread(clientThreadId, handleClient, (selfPtr, client))
+        echo("runServer(), new thread created")
+        let newThread = move(clientThreadId)
+        clients.addLast(newThread)
+        echo("runServer(), thread added to client list")
 
         # Wait until there are at least two nodes
         if clients.len() > 1:
             if not clients[0].running():
-                # Avaoid that sequence gets too large
+                # Avoid that sequence gets too large
                 joinThread(clients.popFirst())
 
     acquire(self.serverLock)
