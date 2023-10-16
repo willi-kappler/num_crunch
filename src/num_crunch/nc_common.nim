da19d1486e9bd790e606d96d5bcbf915c1e2f847
Willi Kappler <willi.kappler.gm@gmail.com>
2023-10-16 20:58:38 +0200

    Add common conversion code

--- src/num_crunch/nc_common.nim
+++ src/num_crunch/nc_common.nim
@@ -1,2 +1,13 @@
 # Common helper functions and types
+from flatty import fromFlatty, toFlatty
+
+from private/nc_message import ncStrToBytes, ncBytesToStr
+
+proc ncToBytes*[T](data: T): seq[byte] =
+    let encStr = toFlatty(data)
+    return ncStrToBytes(encStr)
+
+proc ncFromBytes*[T](data: seq[byte]): T =
+    let encStr = ncBytesToStr(data)
+    return fromFlatty(encStr, T)
 
