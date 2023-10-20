ab1568037455ca34d0802566c0e44386fb27892b
Willi Kappler <willi.kappler.gm@gmail.com>
2023-10-21 00:40:33 +0200

    Use std complex numbers

--- examples/mandelbrot/m_node.nim
+++ examples/mandelbrot/m_node.nim
@@ -1,5 +1,8 @@
 
 
+# Nim imports
+import std/complex
+
 # Local imports
 import num_crunch/nc_common
 
@@ -15,7 +18,7 @@ proc init*(self: var MandelNodeDP, data: seq[byte]) =
     self.initData = initData
     self.data.pixelData = newSeq[uint32](initData.tileWidth * initData.tileHeight)
 
-proc processData*(self: var MandelNodeDP, input: seq[byte]): seq[byte] =
+proc processDataOld*(self: var MandelNodeDP, input: seq[byte]): seq[byte] =
     let (tx, ty) = ncFromBytes(input, (uint32, uint32))
     self.data.tx = tx
     self.data.ty = ty
@@ -56,6 +59,41 @@ proc processData*(self: var MandelNodeDP, input: seq[byte]): seq[byte] =
             self.data.pixelData[(y * tw) + x] = currentIter
             re = re + reStep
         im = im + imStep
+
+    let data = ncToBytes(self.data)
+    return data
+
+proc processData*(self: var MandelNodeDP, input: seq[byte]): seq[byte] =
+    let (tx, ty) = ncFromBytes(input, (uint32, uint32))
+    self.data.tx = tx
+    self.data.ty = ty
+
+    let tw = self.initData.tileWidth
+    let th = self.initData.tileHeight
+    let factorX = float64(tw * tx)
+    let factorY = float64(th * ty)
+    let reStep = self.initData.reStep
+    let imStep = self.initData.imStep
+    let re = self.initData.re1 + (reStep * factorX)
+    let im = self.initData.im1 + (imStep * factorY)
+    let maxIter = self.initData.maxIter
+
+    var currentIter: uint32 = 0
+    var z = complex64(0.0, 0.0)
+
+    for y in 0..<th:
+        for x in 0..<tw:
+            currentIter = 0
+            let c = complex64(re + (float64(x) * reStep), im + (float64(y) * imStep))
+
+            while (z.abs2() <= 4.0) and (currentIter < maxIter):
+                z = (z * z) + c
+                currentIter = currentIter + 1
+
+            if currentIter < maxIter:
+                self.data.pixelData[(y * tw) + x] = currentIter
+            else:
+                self.data.pixelData[(y * tw) + x] = 0
 
     let data = ncToBytes(self.data)
     return data
