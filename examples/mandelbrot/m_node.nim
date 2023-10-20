445150a3839b89bfaf627a1e52999f66913c2ec3
Willi Kappler <willi.kappler@uni-tuebingen.de>
2023-10-20 17:46:25 +0200

    Finish mandel calculation

--- examples/mandelbrot/m_node.nim
+++ examples/mandelbrot/m_node.nim
@@ -27,15 +27,31 @@ proc processData*(self: var MandelNodeDP, input: seq[byte]): seq[byte] =
     let reStep = self.initData.reStep
     let imStep = self.initData.imStep
     let maxIter = self.initData.maxIter
-    var re = 0.0
-    var im = self.initData.im1 + (self.initData.imStep * factorY)
+
     var currentIter: uint32 = 0
+    var re: float64 = 0.0
+    var im: float64 = self.initData.im1 + (self.initData.imStep * factorY)
+    var zre: float64 = 0.0
+    var zim: float64 = 0.0
+    var tmp: float64 = 0.0
+    var a: float64 = 0.0
+    var b: float64 = 0.0
 
     for y in 0..<th:
         re = self.initData.re1 + (reStep * factorX)
         for x in 0..<tw:
             currentIter = 0
-            # TODO: calculate pixel value for Mandelbrot
+            zre = 0.0
+            zim = 0.0
+            a = 0.0
+            b = 0.0
+            while (a + b <= 4.0) and (currentIter < maxIter):
+                a = zre * zre
+                b = zim * zim
+                tmp = a - b + re
+                zim = (2.0 * b) + im
+                zre = tmp
+                currentIter = currentIter + 1
 
             self.data.pixelData[(y * tw) + x] = currentIter
             re = re + reStep
