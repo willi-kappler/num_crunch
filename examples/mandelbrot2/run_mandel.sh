1249a870d44b7294dfdb80bb78413f000e8a4f97
Willi Kappler <willi.kappler@uni-tuebingen.de>
2023-11-23 18:09:58 +0100

    Add two example batch scripts on hoe to run the mandel example on a cluster

---
+++ examples/mandelbrot2/run_mandel.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Run script for num_crunch mandelbrot example.
+# This can be run on a SLURM based cluster (HPC).
+
+# Start the mandel server first and set its ip / hostname in the configuration file.
+
+# You have to adapt these lines to match your cluster partition, e-mail, etc.
+COMMON_OPT="-e mandel_error%j -o mandel_out%j --mail-type=ALL --mail-user=my_email -n 1 -N 1"
+
+srun $COMMON_OPT -J mandel1 ./mandel
+sleep 2
+
+srun $COMMON_OPT -J mandel2 ./mandel
+sleep 2
+
+srun $COMMON_OPT -J mandel3 ./mandel
+sleep 2
+
+srun $COMMON_OPT -J mandel4 ./mandel
